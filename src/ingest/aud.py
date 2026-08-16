2b34d5b9a170fb2bc01efa2725132a215e5ac674
manit <manit17102006@gmail.com>
2026-08-16 14:59:25 +0530

    setup uv

---
+++ src/ingest/aud.py
@@ -0,0 +1,14 @@
+from faster_whisper import WhisperModel
+
+model_size = "large-v2"
+
+# Run on GPU with FP16 apne hardware k hisab se ise change kr lena
+model = WhisperModel(model_size, device="cuda", compute_type="float16")
+
+segments, info = model.transcribe("../../data/videos/output.mp3", beam_size=5)
+
+print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
+
+for segment in segments:
+    print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+
